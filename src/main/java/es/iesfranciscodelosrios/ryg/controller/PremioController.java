4b6e783a0abc959abe1109d5f5bfca570a8509a1
Alvaro L <alvarooleto@gmail.com>
2022-02-05 19:46:35 +0100

    GET premios entregados y no entregados

--- src/main/java/es/iesfranciscodelosrios/ryg/controller/PremioController.java
+++ src/main/java/es/iesfranciscodelosrios/ryg/controller/PremioController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.iesfranciscodelosrios.ryg.model.Boleto;
 import es.iesfranciscodelosrios.ryg.model.Premio;
 import es.iesfranciscodelosrios.ryg.services.PremioService;
 import org.springframework.http.HttpHeaders;
@@ -26,9 +27,8 @@ import org.springframework.http.HttpStatus;
 
 public class PremioController {
 	@Autowired
-    PremioService service;
-	
-	
+	PremioService service;
+
 	/**
 	 * Método que recoge una petición http para hacer una consulta a la base de
 	 * datos y devolver una lista de premios
@@ -36,17 +36,16 @@ public class PremioController {
 	 * @return Lista de premios de la base de datos, lista vacía en caso contrario
 	 */
 	@GetMapping
-    public ResponseEntity<List<Premio>> getAllPremios() {
+	public ResponseEntity<List<Premio>> getAllPremios() {
 		try {
-		    List<Premio> list = service.getAllPremios();
-	        return new ResponseEntity<List<Premio>>(list, new HttpHeaders(), HttpStatus.OK);
+			List<Premio> list = service.getAllPremios();
+			return new ResponseEntity<List<Premio>>(list, new HttpHeaders(), HttpStatus.OK);
 		} catch (Exception e) {
-			 List<Premio> list = new ArrayList<Premio>(); 
-			 return new ResponseEntity<List<Premio>>(list, new HttpHeaders(), HttpStatus.OK);
+			List<Premio> list = new ArrayList<Premio>();
+			return new ResponseEntity<List<Premio>>(list, new HttpHeaders(), HttpStatus.OK);
 		}
-    
-    }
-	
+
+	}
 
 	/**
 	 * Método que recoge una petición http para hacer una consulta a la base de
@@ -56,7 +55,7 @@ public class PremioController {
 	 * @return premio encontrado en la bd, o vacío si hay algún error
 	 */
 	@GetMapping("/{id}")
-    public ResponseEntity<Premio> getPremiosById(@PathVariable("id") Integer id){
+	public ResponseEntity<Premio> getPremiosById(@PathVariable("id") Integer id) {
 		if (id != null && id > -1) {
 			try {
 				Premio entity = service.getPremiosById(id);
@@ -69,78 +68,126 @@ public class PremioController {
 			return new ResponseEntity<Premio>(new Premio(), new HttpHeaders(), HttpStatus.OK);
 		}
 
-    }
+	}
+
+	@GetMapping("/description/{description}")
+	public ResponseEntity<List<Premio>> getPremiosByDescription(@PathVariable("description") String description) {
+		List<Premio> list = service.getPremiosByDescription(description);
+
+		return new ResponseEntity<List<Premio>>(list, new HttpHeaders(), HttpStatus.OK);
+	}
+
+	/**
+	 * Método que recoge una petición http a nuestra API para hacer una consulta a
+	 * la base de datos y devolver todos los Premios entregados.
+	 * 
+	 * @return Lista con todos los premios entregados de la base de datos, una lista
+	 *         vacía si algo ha ido mal
+	 */
+	@GetMapping("/entregado")
+	public ResponseEntity<List<Premio>> getPremiosEntregados(@PathVariable("entregado") boolean entregado) {
+		if (entregado != false) {
+			try {
+				List<Premio> getPremiosEntregados = service.getPremiosEntregados();
+				return new ResponseEntity<List<Premio>>(getPremiosEntregados, new HttpHeaders(), HttpStatus.OK);
+			} catch (Exception e) {
+				List<Premio> getPremiosEntregados = new ArrayList<Premio>();
+				return new ResponseEntity<List<Premio>>(getPremiosEntregados, new HttpHeaders(), HttpStatus.OK);
+			}
+		} else {
+			List<Premio> getPremiosEntregados = new ArrayList<Premio>();
+			return new ResponseEntity<List<Premio>>(getPremiosEntregados, new HttpHeaders(), HttpStatus.OK);
+		}
+
+	}
 	
-	
-	 @GetMapping("/description/{description}")
-	    public ResponseEntity<List<Premio>> getPremiosByDescription(@PathVariable("description") String description)
-	       {
-	    	List <Premio> list = service.getPremiosByDescription(description);
-	 
-	        return new ResponseEntity <List<Premio>>(list, new HttpHeaders(), HttpStatus.OK);
-	    }
-	 /**
-		 * Método que recoge una petición http para hacer una consulta a la base de
-		 * datos y crear un premio nuevo
-		 * 
-		 * @param id
-		 * @return premio encontrado en la bd, o vacío si hay algún error
-		 */
-	 @PostMapping
-	    public ResponseEntity<Premio> createPremios(@Valid @RequestBody Premio myPremio){
-		 if (myPremio != null && myPremio.getId() == -1) {
-				try {
-					Premio created = service.createPremio(myPremio);
-					return new ResponseEntity<Premio>(created, new HttpHeaders(), HttpStatus.OK);
-				} catch (Exception e) {
-					return new ResponseEntity<Premio>(new Premio(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
-				}
-			} else {
+	/**
+	 * Método que recoge una petición http a nuestra API para hacer una consulta a
+	 * la base de datos y devolver todos los Premios no entregados.
+	 * 
+	 * @return Lista con todos los premios no entregados de la base de datos, una lista
+	 *         vacía si algo ha ido mal
+	 */
+	@GetMapping("/no-entregado")
+	public ResponseEntity<List<Premio>> getPremiosNoEntregados(@PathVariable("no-entregado") boolean entregado) {
+		if (entregado != true) {
+			try {
+				List<Premio> getPremiosNoEntregados = service.getPremiosNoEntregados();
+				return new ResponseEntity<List<Premio>>(getPremiosNoEntregados, new HttpHeaders(), HttpStatus.OK);
+			} catch (Exception e) {
+				List<Premio> getPremiosNoEntregados = new ArrayList<Premio>();
+				return new ResponseEntity<List<Premio>>(getPremiosNoEntregados, new HttpHeaders(), HttpStatus.OK);
+			}
+		} else {
+			List<Premio> getPremiosNoEntregados = new ArrayList<Premio>();
+			return new ResponseEntity<List<Premio>>(getPremiosNoEntregados, new HttpHeaders(), HttpStatus.OK);
+		}
+
+	}
+
+	/**
+	 * Método que recoge una petición http para hacer una consulta a la base de
+	 * datos y crear un premio nuevo
+	 * 
+	 * @param id
+	 * @return premio encontrado en la bd, o vacío si hay algún error
+	 */
+	@PostMapping
+	public ResponseEntity<Premio> createPremios(@Valid @RequestBody Premio myPremio) {
+		if (myPremio != null && myPremio.getId() == -1) {
+			try {
+				Premio created = service.createPremio(myPremio);
+				return new ResponseEntity<Premio>(created, new HttpHeaders(), HttpStatus.OK);
+			} catch (Exception e) {
 				return new ResponseEntity<Premio>(new Premio(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
 			}
-	    	
-	        
-	    }
-	 /**
-		 * Método que recoge una petición http para hacer una consulta a la base de
-		 * datos y actualizar un premio
-		 * 
-		 * @param id
-		 * @return premio encontrado en la bd, o vacío si hay algún error
-		 */
-	 @PutMapping
-	    public ResponseEntity<Premio> updatePremio(@Valid @RequestBody Premio myPremio){
-		 if (myPremio != null && myPremio.getId() != -1) {
-				try {
-					Premio updated = service.updatePremio(myPremio);
-			        return new ResponseEntity<Premio>(updated, new HttpHeaders(), HttpStatus.OK);
-				} catch (Exception e) {
-					return new ResponseEntity<Premio>(new Premio(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
-				}
-			} else {
+		} else {
+			return new ResponseEntity<Premio>(new Premio(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	/**
+	 * Método que recoge una petición http para hacer una consulta a la base de
+	 * datos y actualizar un premio
+	 * 
+	 * @param id
+	 * @return premio encontrado en la bd, o vacío si hay algún error
+	 */
+	@PutMapping
+	public ResponseEntity<Premio> updatePremio(@Valid @RequestBody Premio myPremio) {
+		if (myPremio != null && myPremio.getId() != -1) {
+			try {
+				Premio updated = service.updatePremio(myPremio);
+				return new ResponseEntity<Premio>(updated, new HttpHeaders(), HttpStatus.OK);
+			} catch (Exception e) {
 				return new ResponseEntity<Premio>(new Premio(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
 			}
-	    	
-	    }
-	 /**
-		 * Método que recoge una petición http para hacer una consulta a la base de
-		 * datos y borrar un premio
-		 * 
-		 * @param id
-		 * @return premio encontrado en la bd, o vacío si hay algún error
-		 */
-	 @DeleteMapping("/{id}")
-	    public HttpStatus deletePremioById(@PathVariable("id") Integer id){
-		 if (id != null && id > -1) {
-				try {
-					service.deletePremioById(id);
-				} catch (Exception e) {
-					return HttpStatus.BAD_REQUEST;
-				}
-				return HttpStatus.OK;
-			} else {
+		} else {
+			return new ResponseEntity<Premio>(new Premio(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	/**
+	 * Método que recoge una petición http para hacer una consulta a la base de
+	 * datos y borrar un premio
+	 * 
+	 * @param id
+	 * @return premio encontrado en la bd, o vacío si hay algún error
+	 */
+	@DeleteMapping("/{id}")
+	public HttpStatus deletePremioById(@PathVariable("id") Integer id) {
+		if (id != null && id > -1) {
+			try {
+				service.deletePremioById(id);
+			} catch (Exception e) {
 				return HttpStatus.BAD_REQUEST;
-			} 
-	    }
+			}
+			return HttpStatus.OK;
+		} else {
+			return HttpStatus.BAD_REQUEST;
+		}
+	}
 
 }
--- src/main/java/es/iesfranciscodelosrios/ryg/repository/PremioRepository.java
+++ src/main/java/es/iesfranciscodelosrios/ryg/repository/PremioRepository.java
@@ -10,17 +10,15 @@ import es.iesfranciscodelosrios.ryg.model.Premio;
 
 @Repository
 
-public interface PremioRepository extends JpaRepository<Premio,Integer> {
-	 @Query(
-	    		value="SELECT * FROM Premio as p WHERE p.Descripcion LIKE %?1% ",
-	    		nativeQuery = true)
-	    		
-	    		
+public interface PremioRepository extends JpaRepository<Premio, Integer> {
 
-	   
-	    public List<Premio> getByDescripcion(String descripcion);
-	
-	 
-	
+	@Query(value = "SELECT * FROM premio as p WHERE p.Descripcion LIKE %?1% ", nativeQuery = true)
+	public List<Premio> getByDescripcion(String descripcion);
+
+	@Query(value = "SELECT * FROM premio as p WHERE p.entregado = 1", nativeQuery = true)
+	public List<Premio> getPremiosEntregados();
+
+	@Query(value = "SELECT * FROM premio as p WHERE p.entregado = 0", nativeQuery = true)
+	public List<Premio> getPremiosNoEntregados();
 
 }
